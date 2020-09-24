b551d1428bfb11413ca0af96f516017baaa953e6
Sujit <sujitkhopade2001@gmail.com>
2020-09-23 19:11:33 -0700

    Updates

---
+++ precache-manifest.d06cc4de9a9f19dd9ffa903d992f06ae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8f85f11ddcddfc405a098bb27a185ac",
+    "url": "/COVID-19/index.html"
+  },
+  {
+    "revision": "68a32be3962574389066",
+    "url": "/COVID-19/static/css/main.91398512.chunk.css"
+  },
+  {
+    "revision": "9700243aed6baf14c7b5",
+    "url": "/COVID-19/static/js/2.aca830fd.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/COVID-19/static/js/2.aca830fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68a32be3962574389066",
+    "url": "/COVID-19/static/js/main.7a3dacca.chunk.js"
+  },
+  {
+    "revision": "3c3d4e5ed7a08909364a",
+    "url": "/COVID-19/static/js/runtime-main.2a891522.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/COVID-19/static/media/header.d7265326.png"
+  }
+]);
